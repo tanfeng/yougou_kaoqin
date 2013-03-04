f2255cf1a82df9fd019e8d83512e374f9c15b412
minxr <minxiaorong@126.com>
2013-03-04 16:34:40 +0800

    初始化了项目，没有做任何定制

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YougouKaoqin::Application.config.secret_token = '0e665a7db9e7aa6d066ea64f713f060d0fd606c016d49b1ec928a9a25b91d276f0c97ef545921f28f2363d92ec8a97801a50cf16775f03fe6d056dc0fe24e3e7'
